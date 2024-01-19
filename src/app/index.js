620d3449d24869a0a51d523efca324fd20105d7a
Xglbrilliant <brilliant2125@163.com>
2024-01-19 19:36:38 +0800

    use koa-bodyparser

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,10 @@
 const koa = require('koa')
+const bodyparser = require('koa-bodyparser')
 const { userRouter } = require('../router/user')
 
 const app = new koa()
 
+app.use(bodyparser())
 app.use(userRouter.routes())
 app.use(userRouter.allowedMethods())
 
