5cf43a3de5f7656d9282f0df5e5e3d0e3049bff5
Xglbrilliant <brilliant2125@163.com>
2024-01-19 20:19:00 +0800

    verify user registration information

--- src/router/user.js
+++ src/router/user.js
@@ -10,6 +10,24 @@ userRouter.post('/register', async (ctx, next) => {
     const { name, password } = ctx.request.body//?1、获取用户的数据
     console.log(name, password);
 
+    // ! 验证用户信息的逻辑
+    if(!name || !password) {//todo:判断用户名或密码是否为空
+        ctx.body = {
+            code: -1001,
+            message: '用户名或密码不能为空!'
+        }
+        return
+    }
+    const statement1 = 'SELECT * FROM `user` WHERE name = ?;'//todo:判断用户名是否已经存在
+    let [users] = await connection.execute(statement1, [name])//返回查询到的用户结果
+    if(users.length) {//如果查询到的数组不为空，说明查询到了用户数据(即用户名已经存在)
+        ctx.body = {
+            code: -1002,
+            message: '用户名已经存在,请重新注册!'
+        }
+        return
+    }
+    
     //?2、拼接statement
     const statement = 'INSERT INTO `user` (name, password) VALUES (?, ?);'
 
