54949aec9fd58027b842be59629b46f77d5a0cf4
Xglbrilliant <brilliant2125@163.com>
2024-01-19 20:02:04 +0800

    test add user data to database

--- src/router/user.js
+++ src/router/user.js
@@ -4,11 +4,24 @@ const connection = require('../app/database')
 const userRouter = new koaRouter({ prefix: '/users' })
 
 // 用户注册接口
-userRouter.post('/login', (ctx, next) => {
-    const userInfo = ctx.request.body
-    console.log(userInfo);
+userRouter.post('/register', async (ctx, next) => {
+    // const userInfo = ctx.request.body
+    // console.log(userInfo);
+    const { name, password } = ctx.request.body//?1、获取用户的数据
+    console.log(name, password);
 
-    ctx.body = '注册成功~'
+    //?2、拼接statement
+    const statement = 'INSERT INTO `user` (name, password) VALUES (?, ?);'
+
+    // ? 3、执行sql语句
+    let [result] = await connection.execute(statement, [name, password])//异步操作
+
+    ctx.body = {
+        message: '注册成功~',
+        data: result
+    }
+
+    return result
 })
 
 module.exports = {
