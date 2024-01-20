45a2dc0a63a8bc9953f459c7de4422ba2517c2f1
Xglbrilliant <brilliant2125@163.com>
2024-01-20 15:11:16 +0800

    user interface

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,15 @@
 const koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 const { userRouter } = require('../router/user')
+const { loginRouter } = require('../router/userLogin')
 
 const app = new koa()
 
 app.use(bodyparser())
 app.use(userRouter.routes())
 app.use(userRouter.allowedMethods())
+app.use(loginRouter.routes())
+app.use(loginRouter.allowedMethods())
 
 module.exports = app
 
