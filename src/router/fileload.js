3b1efe0898be1d61e60b2302ea270c8aa1470617
Xglbrilliant <brilliant2125@163.com>
2024-01-21 20:47:18 +0800

    files load success

--- src/router/fileload.js
+++ src/router/fileload.js
@@ -1,6 +1,7 @@
 const koaRouter = require('@koa/router')
 const multer = require('@koa/multer')
 const { verifyAuth } = require('../middleware/verifyLoginAuth')
+const connection = require('../app/database')
 
 const uploadAvatar = multer({
     dest: './uploads'
@@ -8,10 +9,20 @@ const uploadAvatar = multer({
 
 const fileRouter = new koaRouter({ prefix: '/files' })
 
-fileRouter.post('/avatar', verifyAuth, uploadAvatar.single('avatar'), (ctx, next) => {
-    console.log(ctx.request.file);
+fileRouter.post('/avatar', verifyAuth, uploadAvatar.single('avatar'), async (ctx, next) => {
+    // console.log(ctx.request.file);
+    // console.log(ctx.users);
+    const { filename, mimetype, size } = ctx.request.file
+    const { id } = ctx.users
+    const statement = 'INSERT INTO avatar (filename, mimetype, size, user_id) VALUES (?, ?, ?, ?);'
+    const [result] = await connection.execute(statement, [filename, mimetype, size, id])
+    console.log(result);
 
-    ctx.body = '文件上传成功~'
+    ctx.body = {
+        code: 0,
+        message: '文件上传成功',
+        data: result
+    }
 })
 
 module.exports = fileRouter
